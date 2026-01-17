d437fd9b8b0674df499e3344a7e8c0a394091ea1
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2026-01-16 19:53:46 -0500

    fix: common html entities (e.g. apostrophes) in post text unsearchable

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -1,5 +1,13 @@
 export const tokenizer = {
 	tokenize(text: string) {
-		return text.split(/\s/).filter((i) => i);
+		return text
+			.replaceAll('&#39;', "'")
+			.replaceAll('&apos;', "'")
+			.replaceAll('&quot;', '"')
+			.replaceAll('&amp;', '&')
+			.replaceAll('&lt;', '<')
+			.replaceAll('&gt;', '>')
+			.split(/\s/)
+			.filter((i) => i);
 	},
 };
